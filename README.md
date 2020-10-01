642c03e061ab168f01997732769a7d3b503ed23a
Yannick Stachelscheid <yannickstach@googlemail.com>
2020-10-01 16:18:49 +0200

    move assets from old repo

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# eslint-config
-Base eslint config for all addbots projects
+# @addbots/eslint-config
+
+Default eslint config for all addbots public and private projects.
+
+## Usage
+`npm install --save-dev @addbots/eslint-config`
+
+With peer dependecies
+
+`npm install --save-dev @addbots/eslint-config @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-react eslint-plugin-react-hooks`
+
+## License
+This project is licensed under the [MIT](LICENSE) license.
---
+++ index.js
@@ -0,0 +1,65 @@
+module.exports = {
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: "module",
+		ecmaFeatures: {
+			modules: true,
+		},
+	},
+	extends: [
+		"eslint:recommended",
+		"plugin:react/recommended",
+		"prettier", // disables rules that conflict with prettier
+	],
+	plugins: ["@typescript-eslint", "react-hooks"],
+	rules: {
+		"no-multiple-empty-lines": [
+			"error",
+			{
+				max: 1,
+				maxEOF: 1,
+				maxBOF: 0,
+			},
+		],
+		"comma-dangle": [
+			"error",
+			{
+				arrays: "always-multiline",
+				objects: "always-multiline",
+				imports: "ignore",
+				exports: "ignore",
+				functions: "ignore",
+			},
+		],
+		"no-duplicate-imports": [
+			"error",
+			{
+				includeExports: true,
+			},
+		],
+		"no-unused-vars": 0, // disable because @typescript-eslint/no-unused-vars does the job
+		"@typescript-eslint/no-unused-vars": 2,
+		"no-undef": 0,
+		"no-extra-parens": 0, // disable unnecessary parentheses
+		"no-import-assign": 2, // disallow assigning to imported bindings
+		"no-useless-escape": 0, // disable error on unnecessary escape usage in regex
+		"no-unreachable": 0,
+		"no-dupe-class-members": 0,
+		"require-atomic-updates": 0,
+		"no-inner-declarations": 0,
+		"@typescript-eslint/no-floating-promises": 2,
+
+		// react specific rules
+		"react/prop-types": "off",
+		"react/display-name": "off",
+		"react/no-find-dom-node": "warn",
+		"react-hooks/rules-of-hooks": "error",
+		"react-hooks/exhaustive-deps": "warn",
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
+}
